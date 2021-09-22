a9ecc10e37ed62b0893a79f1d4a475615bf8849e
Davide Scalisi <davide.scalisi@gmail.com>
2021-09-22 08:54:16 +0200

    draft

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,76 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    // Start() and Update() methods deleted - we don't need them right now
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    public static MainManager Instance;
+    public Color TeamColor; // new variable declared
 
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        // start of new code
+        if (Instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+        // end of new code
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
 
-    private void Update()
+    [System.Serializable] //per salvare con json
+    class SaveData // per salvare i dati dell'utente
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+        public Color TeamColor; //il colore che lo user seleziona
     }
 
-    void AddPoint(int point)
+    public void SaveColor() //metodo per salvare il dato
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        //creo un'istanza dei dati salvati e riempio la variabile team color
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        // trasformo l'istanza in un json con l'utility, creo json e gli butto dentro data
+        string json = JsonUtility.ToJson(data);
+
+        // metodo per scrivere il file, con il path e la variabile da scrivere ovvero json
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
 
-    public void GameOver()
+    public void LoadColor()
     {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }
